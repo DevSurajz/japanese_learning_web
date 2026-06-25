3c2b1d951d7058989394f5ba429637fbd5b16ae3
Suraj <ssharma50152@gmail.com>
2026-06-25 13:03:03 +0530

    new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${cormorant.variable} ${spaceGrotesk.variable} ${notoSansJP.variable}`}>
       <head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/jpeg" href="/favicon.jpg" />
       </head>
       <body>
         <ProgressBar />
